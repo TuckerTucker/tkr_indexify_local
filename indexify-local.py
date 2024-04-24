51d1f15a8f163a0d038c3e9c6d26bf5bc7a10c01
Tucker <tucker@fakeemail.com>
2024-04-24 01:11:31 -0600

    added indexify-init-config create in indexify-local.py

--- indexify-local.py
+++ indexify-local.py
@@ -80,6 +80,7 @@ def setup():
 
     # Download the all_extractors.json file
     download_all_extractors()
+    
 def generate_supervisord_conf(input_file="indexify-local/extractors-json/local_extractors.json", output_file="supervisord.conf"):
     try:
         with open(input_file, "r") as file:
@@ -133,6 +134,26 @@ def generate_supervisord_conf(input_file="indexify-local/extractors-json/local_e
     except IOError as e:
         print(f"Error writing to file: {e}")
 
+def create_indexify_config(template_path, output_path):
+    # Read the template file
+    with open(template_path, 'r') as template_file:
+        template_content = template_file.read()
+
+    # Get the full path of the destination directory
+    destination_dir = os.path.dirname(os.path.abspath(output_path))
+
+    # Replace the '../' placeholders with the full path of the destination directory
+    updated_content = template_content.replace('../', destination_dir + '/')
+
+    # Create the output directory if it doesn't exist
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    # Write the updated content to the output file
+    with open(output_path, 'w') as output_file:
+        output_file.write(updated_content)
+
+    print(f"Indexify configuration file created at: {output_path}")
+
 def main():
     parser = argparse.ArgumentParser(description="Extractor Downloader")
     parser.add_argument("--all-extractors", action="store_true", help="Download all extractors from the default GitHub URL")
@@ -140,6 +161,7 @@ def main():
     parser.add_argument("--start", metavar="SCRIPT", nargs="?", const=True, help="Path to the bash script to execute (default: run start_env and start_xfi)")
     parser.add_argument("--setup", action="store_true", help="Run the setup process (get_latest.sh and download all_extractors.json)")
     parser.add_argument("--make-conf", action="store_true", help="Generate supervisord.conf based on local-extractors.json")
+    parser.add_argument("--make-init-config", action="store_true", help="Generate indexify.init.config based on the template")
     args = parser.parse_args()
 
     if args.setup:
@@ -156,6 +178,10 @@ def main():
             run_bash_script(bash_script_path)
     elif args.make_conf:
         generate_supervisord_conf()
+    elif args.make_init_config:
+        template_path = 'utils/indexify.init.config.template'
+        output_path = 'indexify-local/indexify.init.config'
+        create_indexify_config(template_path, output_path)
     else:
         parser.print_help()
 
