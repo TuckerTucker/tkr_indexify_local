2818ac224f12a48d7a79aa9e15cdf43758e55d6b
Tucker <tucker@fakeemail.com>
2024-04-24 23:57:40 -0600

    moved _local_data 	modified:   .gitignore 	modified:   indexify-local.py 	deleted:    indexify-local/indexify.init.config 	modified:   utils/indexify.init.config.template

--- indexify-local.py
+++ indexify-local.py
@@ -139,10 +139,10 @@ def create_indexify_config(template_path, output_path):
     with open(template_path, 'r') as template_file:
         template_content = template_file.read()
 
-    # Get the full path of the destination directory
-    destination_dir = os.path.dirname(os.path.abspath(output_path))
+    # Get the full path of the destination directory's parent directory
+    destination_dir = os.path.dirname(os.path.abspath(os.path.dirname(output_path)))
 
-    # Replace the '../' placeholders with the full path of the destination directory
+    # Replace the '../' placeholders with the full path of the destination directory's parent directory
     updated_content = template_content.replace('../', destination_dir + '/')
 
     # Create the output directory if it doesn't exist
