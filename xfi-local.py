af0da3f5d933420ce69468cae0fd50667176995f
Tucker <tucker@fakeemail.com>
2024-05-09 01:51:05 -0600

    update: Setup process

--- xfi-local.py
+++ xfi-local.py
@@ -1,9 +1,9 @@
-import subprocess
 import argparse
+import logging
+import os
+import subprocess
 import requests
 import time
-import os
-import logging
 from typing import NoReturn
 from xfi.setup import setup, reset, clear_logs
 from xfi.extractor_downloader import download_all_extractors, download_extractors
@@ -15,44 +15,41 @@ from xfi.new_local import NewLocal
 from xfi.local_logger import configure_logging
 
 def check_file_exists(file_path: str) -> None:
+    """Check if a file exists at the specified path."""
     if not os.path.exists(file_path):
         logging.error(f"We Checked, it doesn't exist: {file_path}")
         raise FileNotFoundError(f"We Checked, it doesn't exist: {file_path}")
     else:
         logging.info(f"Yep, it's there: {file_path}")
-       
-    
+
 def main() -> NoReturn:
+    """Main function to parse arguments and handle the program flow based on user input."""
     configure_logging()
     logging.info("Starting main function")
 
     parser = argparse.ArgumentParser(description="xfi local")
+    parser.add_argument("--setup", nargs='?', const='whisper_chain.json', metavar='FILE', help="Run the setup process with the specified file or default to 'whisper_chain.json'")
+    parser.add_argument("--reset", action="store_true", help="Removes all logs and config files")
+    parser.add_argument("--clear-logs", action="store_true", help="Removes all logs")
     parser.add_argument("--all-extractors", action="store_true", help="Download all extractors from the default GitHub URL")
     parser.add_argument("--local-extractors", metavar="INPUT_FILE", help="Download extractors from the specified JSON file")
     parser.add_argument("--start", metavar="SCRIPT", nargs="?", const=True, help="Path to the bash script to execute (default: run start_env and start_xfi)")
-    parser.add_argument("--setup", action="store_true", help="Run the setup process (get_latest.sh and download all_extractors.json)")
     parser.add_argument("--make-conf", action="store_true", help="Generate supervisord.conf based on local-extractors.json")
     parser.add_argument("--make-init-config", action="store_true", help="Generate indexify.init.config based on the template")
     parser.add_argument("--new-local", metavar="RECIPE_PATH", help="Path to the new_recipe.json file")
-    parser.add_argument("--reset", action="store_true", help="Removes all logs and config files")
-    parser.add_argument("--clear-logs", action="store_true", help="Removes all logs")
-                        
+
     args = parser.parse_args()
 
     try:
         if args.setup:
-            setup()
+            setup(args.setup)
         elif args.reset:
-            reset()      
+            reset()
         elif args.clear_logs:
-            clear_logs()                        
+            clear_logs()
         elif args.all_extractors:
             download_all_extractors()
         elif args.local_extractors:
-            if args.local_extractors is None:
-                logging.error("Input file for local extractors must be specified.")
-                print("Error: Input file for local extractors must be specified.")
-                return
             download_extractors(input_file=args.local_extractors)
         elif args.new_local:
             new_local = NewLocal(args.new_local)
@@ -83,15 +80,12 @@ def main() -> NoReturn:
             logging.info(input_file_path)
             full_path = os.path.abspath(input_file_path[0]),
             logging.info(full_path[0])
-            try:
-                check_file_exists(full_path[0])
-                generate_supervisord_conf(
-                    input_file = full_path[0],
-                    output_file=templates['supervisord']['output_path'],
-                    template_file=templates['supervisord']['template_path']
-                )
-            except FileNotFoundError as e:
-                print(e)
+            check_file_exists(full_path[0])
+            generate_supervisord_conf(
+                input_file=full_path[0],
+                output_file=templates['supervisord']['output_path'],
+                template_file=templates['supervisord']['template_path']
+            )
         elif args.make_init_config:
             templates = load_templates()
             create_indexify_config(
@@ -104,8 +98,6 @@ def main() -> NoReturn:
         logging.error(f"An error occurred: {str(e)}")
     finally:
         logging.info("Ending main function")
-
-
 
 if __name__ == "__main__":
     main()
--- xfi/setup.py
+++ xfi/setup.py
@@ -1,24 +1,56 @@
 import subprocess
 import logging
+import os
 from typing import NoReturn
-from .extractor_downloader import download_all_extractors
+from .extractor_downloader import download_all_extractors, download_extractors
 from .local_logger import configure_logging
+from .templates import load_templates
+from xfi.supervisord_conf_generator import generate_supervisord_conf
+from xfi.indexify_config_generator import create_indexify_config
 
-# Configure logging
-configure_logging()
+configure_logging() # Configure logging
 
-def setup() -> NoReturn:
+def setup(chain_file: str = "whisper_chain.json") -> NoReturn:
     """
-    Run the indexify-local/get_latest script and download the all_extractors.json file.
+    Run the indexify-local/get_latest script and download the specified chain file.
+    
+    Args:
+    chain_file (str): The filename of the chain file to download. Defaults to "whisper_chain.json".
     """
     try:
         # Run the get_latest script using subprocess with the specified working directory
         subprocess.run(["bash", "get_latest"], cwd="indexify-local", check=True)
         logging.info("get_latest script executed successfully")
+
     except subprocess.CalledProcessError as e:
         logging.error(f"Error executing get_latest script: {e}")
         raise RuntimeError(f"Error executing get_latest script: {e}")
+    
+    # Download the specified chain file
+    download_extractors(chain_file)
+    logging.info(f"Downloaded extractors from {chain_file} successfully")
 
+    # Generate the config files
+    templates = load_templates() # Load the templates
+
+    input_file_list = templates['supervisord']['input_file']
+    input_file_path = input_file_list  # This gets the string 'input_file'
+    logging.info(input_file_path)
+    
+    full_path = os.path.abspath(input_file_path)
+    logging.info(full_path)
+    
+    generate_supervisord_conf(
+        input_file=full_path,
+        output_file=templates['supervisord']['output_path'],
+        template_file=templates['supervisord']['template_path']
+    )
+
+    create_indexify_config(
+        template_file=templates['indexify']['template_path'],
+        output_file=templates['indexify']['output_path']
+    )
+    
     # Download the all_extractors.json file
     download_all_extractors()
 
