7b2744cdf638e8c629f611e9bf73e643b94bb722
Tucker <tucker@fakeemail.com>
2024-05-10 09:58:44 -0600

    update: install req in --setup

--- xfi/setup.py
+++ xfi/setup.py
@@ -8,37 +8,46 @@ from .templates import load_templates
 from xfi.supervisord_conf_generator import generate_supervisord_conf
 from xfi.indexify_config_generator import create_indexify_config
 
-configure_logging() # Configure logging
+configure_logging()  # Configure logging
+
+def install_requirements() -> None:
+    """
+    Installs the Python packages specified in the requirements.txt file without using the cache.
+    """
+    try:
+        subprocess.run(['pip', 'install', '--no-cache-dir', '-r', 'requirements.txt'], check=True)
+        logging.info("Successfully installed packages from requirements.txt without using cache")
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Failed to install packages: {e}")
+        raise RuntimeError(f"Failed to install packages: {e}")
 
 def setup(chain_file: str = "whisper_chain.json") -> NoReturn:
     """
-    Run the indexify-local/get_latest script and download the specified chain file.
+    Setup the environment by installing dependencies, running scripts, and configuring files.
     
     Args:
-    chain_file (str): The filename of the chain file to download. Defaults to "whisper_chain.json".
+        chain_file (str): The filename of the chain file to download. Defaults to "whisper_chain.json".
     """
+    # Install required Python packages
+    install_requirements()
+    
+    # Get the list of extractors from indexify github
+    download_all_extractors()
+
     try:
-        # Run the get_latest script using subprocess with the specified working directory
         subprocess.run(["bash", "get_latest"], cwd="indexify-local", check=True)
         logging.info("get_latest script executed successfully")
-
     except subprocess.CalledProcessError as e:
         logging.error(f"Error executing get_latest script: {e}")
         raise RuntimeError(f"Error executing get_latest script: {e}")
     
-    # Download the specified chain file
     download_extractors(chain_file)
     logging.info(f"Downloaded extractors from {chain_file} successfully")
 
-    # Generate the config files
-    templates = load_templates() # Load the templates
-
+    templates = load_templates()
     input_file_list = templates['supervisord']['input_file']
-    input_file_path = input_file_list  # This gets the string 'input_file'
-    logging.info(input_file_path)
-    
+    input_file_path = input_file_list
     full_path = os.path.abspath(input_file_path)
-    logging.info(full_path)
     
     generate_supervisord_conf(
         input_file=full_path,
@@ -53,14 +62,10 @@ def setup(chain_file: str = "whisper_chain.json") -> NoReturn:
     
     try:
         subprocess.run(["bash", "setup_ui"], cwd="utils", check=True)
-        logging.info("install ui successful")
+        logging.info("UI setup successful")
     except subprocess.CalledProcessError as e:
-        logging.error(f"Error installing ui: {e}")
-        raise RuntimeError(f"Error installing ui: {e}")
-
-
-    # Download the all_extractors.json file
-    download_all_extractors()
+        logging.error(f"Error setting up UI: {e}")
+        raise RuntimeError(f"Error setting up UI: {e}")
 
 def reset() -> NoReturn:
     """
